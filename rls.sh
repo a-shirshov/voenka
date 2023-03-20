69807732a0940ae0eb0082d63767d7c715ff046b
Artyom Shirshov <artyomsh01@yandex.ru>
2023-03-20 20:39:23 +0300

    rls fix

--- rls.sh
+++ rls.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
-#set -x
-#Координаты точки
-x_point=4
-y_point=4
-
-#Координаты круга
-x_center=3
-y_center=3
-
-#Радиус круга
-radius=3
-
-#Углы сектора
-start_angle=10
-end_angle=45
-
-#Попадает ли точка в радиус круга
-distance=$(echo "sqrt(($x_point-$x_center)^2+($y_point-$y_center)^2)" | bc -l)
-
-echo $distance
-
-if [ $(echo "$distance > $radius" | bc ) -eq 1 ]; then
-    echo "Точка вне круга"
-    exit 0
-fi
-
-x_from_center=$((x_point - x_center))
-y_from_center=$((y_point - y_center))
 
 arccos () {
     scale=3
@@ -49,10 +21,9 @@ getAngle() {
     
     ox_x=1
     ox_y=0
-
-    cos_angle=$(echo "($local_point_x*$ox_x + $local_point_y*$ox_y)/(sqrt($local_point_x^2 + $local_point_y^2)*sqrt($local_point_y^2 + $ox_y^2))" | bc -l)
+    cos_angle=$(echo "($local_point_x*$ox_x + $local_point_y*$ox_y)/(sqrt($local_point_x^2 + $local_point_y^2)*sqrt($ox_x^2 + $ox_y^2))" | bc -l)
     angle=$(arccos $cos_angle)
-    degree_angle=$(echo "$angle*180/3.141592" | bc -l)
+    degree_angle=$(echo "$angle*180/3.1415926535" | bc -l)
     echo $degree_angle
 }
 
@@ -80,32 +51,70 @@ is_in_sector() {
     fi
 }
 
-if [ $x_from_center -ge 0 ] && [ $y_from_center -ge 0 ]; then
-  echo "Точка лежит в первой четверти"
-  angle=$(getAngle $x_from_center $y_from_center)
-  echo $angle
-  found_point=$(is_in_sector $angle)
-  echo $found_point
-elif [ $x_from_center -le 0 ] && [ $y_from_center -ge 0 ]; then
-  echo "Точка лежит во второй четверти"
-  angle=$(getAngle $x_from_center $y_from_center)
-  echo $angle
-  found_point=$(is_in_sector $angle)
-  echo $found_point
-elif [ $x_from_center -le 0 ] && [ $y_from_center -le 0 ]; then
-  echo "Точка лежит в третьей четверти"
-  angle=$(getAngle $x_from_center $y_from_center)
-  angle=$(echo "180 + (180-$angle)" | bc -l)
-  echo $angle
-  found_point=$(is_in_sector $angle)
-  echo $found_point
-elif [ $x_from_center -ge 0 ] && [ $y_from_center -le 0 ]; then
-  echo "Точка лежит в четвертой четверти"
-  angle=$(getAngle $x_from_center $y_from_center)
-  angle=$(echo "360-$angle" | bc -l)
-  echo $angle
-  found_point=$(is_in_sector $angle)
-  echo $found_point
-else
-  echo "Ошибка"
-fi
+#Координаты круга
+x_center=7000000
+y_center=5000000
+
+#Радиус круга
+radius=7000000
+
+#Углы сектора
+start_angle=0
+end_angle=90
+
+DirectoryTargets=/home/artyom/kr_vko/temp/GenTargets/Targets/
+while :
+do
+  sleep 10
+  for file in $(ls $DirectoryTargets -t | head -n 10)
+  do
+    echo $file
+    cat $DirectoryTargets$file
+    file_line=$(cat $DirectoryTargets$file)
+    x_point=${file_line:1:7}
+    y_point=${file_line:10:18}
+
+    distance=$(echo "sqrt(($x_point-$x_center)^2+($y_point-$y_center)^2)" | bc -l)
+
+    echo $distance
+
+    if [ $(echo "$distance > $radius" | bc ) -eq 1 ]; then
+        echo "Точка вне круга"
+        continue
+    fi
+
+    x_from_center=$((x_point - x_center))
+    y_from_center=$((y_point - y_center))
+
+    #Попадает ли точка в радиус круга
+    if [ $x_from_center -ge 0 ] && [ $y_from_center -ge 0 ]; then
+      echo "Точка лежит в первой четверти"
+      angle=$(getAngle $x_from_center $y_from_center)
+      echo $angle
+      found_point=$(is_in_sector $angle)
+      echo $found_point
+    elif [ $x_from_center -le 0 ] && [ $y_from_center -ge 0 ]; then
+      echo "Точка лежит во второй четверти"
+      angle=$(getAngle $x_from_center $y_from_center)
+      echo $angle
+      found_point=$(is_in_sector $angle)
+      echo $found_point
+    elif [ $x_from_center -le 0 ] && [ $y_from_center -le 0 ]; then
+      echo "Точка лежит в третьей четверти"
+      angle=$(getAngle $x_from_center $y_from_center)
+      angle=$(echo "360-$angle" | bc -l)
+      echo $angle
+      found_point=$(is_in_sector $angle)
+      echo $found_point
+    elif [ $x_from_center -ge 0 ] && [ $y_from_center -le 0 ]; then
+      echo "Точка лежит в четвертой четверти"
+      angle=$(getAngle $x_from_center $y_from_center)
+      angle=$(echo "360-$angle" | bc -l)
+      echo $angle
+      found_point=$(is_in_sector $angle)
+      echo $found_point
+    else
+      echo "Ошибка"
+    fi
+  done
+done
